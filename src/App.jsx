b5ec2171580379754f09ae0d997f14d0434d1821
Max Welin <maxwwelin@gmail.com>
2025-09-09 10:53:11 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-full flex place-content-center">
+        <h1 className="text-5xl">Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
