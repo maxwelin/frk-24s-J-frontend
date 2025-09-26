95417ef1f376c484a5a7625fc0e726df51bea6a8
juliansandstrom <juliiansandstrom@gmail.com>
2025-09-26 05:21:59 +0200

    removed color props and instead passing playerTurn

--- src/context/MainContext.jsx
+++ src/context/MainContext.jsx
@@ -5,14 +5,12 @@ const MainContext = createContext(null);
 function MainProvider({ children }) {
   const [playerTurn, setPlayerTurn] = useState(1);
 
-  const currentColor = playerTurn === 1 ? "black" : "white";
-
   const handleCellClick = () => {
     setPlayerTurn((turn) => (turn === 1 ? 2 : 1));
   };
 
   return (
-    <MainContext.Provider value={{ handleCellClick, currentColor }}>
+    <MainContext.Provider value={{ handleCellClick, playerTurn }}>
       {children}
     </MainContext.Provider>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import { Board } from "gumoku-component";
+
 import "./HomePage.css";
+import { useMainContext } from "../hooks/useMainContext";
 
 export default function HomePage() {
+  const { handleCellClick, playerTurn } = useMainContext();
+
   return (
     <div>
       <Board
         boardTiles={80}
         gameStarted={true}
-        playerOrder={"Player 1"}
+        playerTurn={playerTurn}
         largeMenu={false}
         gameEnded={false}
         handleCellClick={handleCellClick}
