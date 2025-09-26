31c147fa624f337786ee39b50bc1a3decb1af36c
juliansandstrom <juliiansandstrom@gmail.com>
2025-09-26 05:14:35 +0200

    deleted extra code inside Homepage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,9 @@
 import { Board } from "gumoku-component";
-import { useState } from "react";
 import "./HomePage.css";
-import { useMainContext } from "../hooks/useMainContext";
 
 export default function HomePage() {
-  const { handleCellClick } = useMainContext();
-
   return (
-    <div onClick={handleCellClick}>
+    <div>
       <Board
         boardTiles={80}
         gameStarted={true}
