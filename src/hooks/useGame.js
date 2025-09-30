952d370b7a6af52b05c447d6be42cf7fa37becf8
juliansandstrom <juliiansandstrom@gmail.com>
2025-09-30 14:52:02 +0200

    removed backend from homepage

--- src/hooks/useGame.js
+++ src/hooks/useGame.js
@@ -1,61 +1,18 @@
 import { useCallback, useEffect, useState } from "react";
 
-const BASE = "http://localhost:3001/api/gomoku";
+const BASE = "http://localhost:3000";
 
 export function useGame() {
-  const [games, setGames] = useState(null);
-  const [creating, setCreating] = useState(false);
+  const [state, setState] = useState(null);
 
-  const fetchGames = useCallback(async () => {
+  const fetchState = useCallback(async () => {
     try {
-      const res = await fetch(`${BASE}/games`, {
-        method: "GET",
-      });
-      const data = await res.json();
-      setGames(data);
-      console.log("game state", data);
-    } catch (e) {
-      console.log("fetchGames fail:", e);
+      const res = await fetch(`${BASE}/api/games`);
+      const data = res.json();
+      setState(data);
+      console.log("Game state:", data);
+    } catch (err) {
+      console.log(err);
     }
-  }, []);
-
-  const findGame = useCallback(async (gameId) => {
-    if (!gameId) throw new Error("findGame: missing gameId");
-    const res = await fetch(`${BASE}/game/${encodeURIComponent(gameId)}`);
-
-    if (!res.ok) throw new Error(`${res.status}`);
-
-    return res.json();
-  }, []);
-
-  const createGame = useCallback(async () => {
-    try {
-      setCreating(true);
-      const res = await fetch(`${BASE}/games/add`, {
-        method: "GET",
-      });
-      const newGame = await res.json();
-
-      await fetchGames();
-      return newGame;
-    } catch {
-      console.log("Create game error");
-    } finally {
-      setCreating(false);
-    }
-  }, [fetchGames]);
-
-  useEffect(() => {
-    fetchGames();
-
-    const time = setInterval(() => {
-      fetchGames();
-    }, 10000);
-
-    return () => {
-      clearInterval(time);
-    };
-  }, [fetchGames]);
-
-  return { games, createGame, creating, findGame };
+  });
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,33 +3,12 @@ import { Board } from "gumoku-component";
 import "./HomePage.css";
 import { gameConfig } from "../config/gameConfig";
 import { useMainContext } from "../hooks/useMainContext";
-import { useGame } from "../hooks/useGame";
-import { useEffect } from "react";
 
 export default function HomePage() {
-  const { games, createGame, creating, findGame } = useGame();
   const { handleCellClick, playerTurn } = useMainContext();
-
-  useEffect(() => {
-    if (!Array.isArray(games) || games.length === 0) return;
-    (async () => {
-      try {
-        const game = await findGame("015cdc04-4d22-46f7-8d8e-f1879bb9bf1b");
-        console.log("findGame", game);
-      } catch (err) {
-        console.error(err);
-      }
-    })();
-  }, [games, findGame]);
 
   return (
     <div>
-      <div>
-        <button onClick={createGame} disabled={creating}>
-          {creating ? "Creating..." : "New Game"}
-        </button>
-      </div>
-
       <Board
         {...gameConfig}
         handleCellClick={handleCellClick}
