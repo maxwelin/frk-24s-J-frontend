69daa80f159307635fa8d2524257740586d7c563
juliansandstrom <juliiansandstrom@gmail.com>
2025-10-02 13:01:56 +0200

    t

--- src/context/MainContext.jsx
+++ src/context/MainContext.jsx
@@ -1,27 +1,46 @@
 import { createContext, useState } from "react";
-const BASE = "http://localhost:3001/api/gomoku";
+
 const MainContext = createContext(null);
 
 function MainProvider({ children }) {
   const [playerTurn, setPlayerTurn] = useState(1);
   const [openModal, setOpenModal] = useState(true);
+  const [brickColor, setBrickColor] = useState("");
+  const [activeCell, setActiveCell] = useState(false);
 
   const toggleModal = (e) => {
     e.stopPropagation();
     setOpenModal(!openModal);
   };
 
-  const handleCellClick = () => {
+  const handleCellClick = (id) => {
+    if (activeCell) return console.log("already activated");
+    const placingColor = playerTurn === 1 ? "black" : "white";
+    console.log("Clicked cell:", id, "color:", placingColor);
+    setBrickColor(placingColor);
     setPlayerTurn((turn) => (turn === 1 ? 2 : 1));
+    setActiveCell(true);
   };
 
   return (
+<<<<<<< HEAD
     <MainContext.Provider value={{ 
       handleCellClick, 
       playerTurn,
       openModal,
       toggleModal
       }}>
+=======
+    <MainContext.Provider
+      value={{
+        handleCellClick,
+        playerTurn,
+        openModal,
+        closeModal,
+        brickColor,
+      }}
+    >
+>>>>>>> e8d7b6d (passing props from handleCellClick in maincontext)
       {children}
     </MainContext.Provider>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,16 +5,26 @@ import { gameConfig } from "../config/gameConfig";
 import { useMainContext } from "../hooks/useMainContext";
 
 export default function HomePage() {
+<<<<<<< HEAD
   const { handleCellClick, playerTurn, openModal, toggleModal } = useMainContext();
+=======
+  const { brickColor, handleCellClick, playerTurn, openModal, closeModal } =
+    useMainContext();
+>>>>>>> e8d7b6d (passing props from handleCellClick in maincontext)
 
   return (
     <div>
       <BackgroundBanner playerTurn={playerTurn} useTurnText />
+<<<<<<< HEAD
       <Menu className={true} openModal={openModal} toggleModal={toggleModal} />
+=======
+      {/* <Menu className={true} openModal={openModal} closeModal={closeModal} /> */}
+>>>>>>> e8d7b6d (passing props from handleCellClick in maincontext)
       <Board
         {...gameConfig}
         handleCellClick={handleCellClick}
         playerTurn={playerTurn}
+        brickColor={brickColor}
       />
       <Button draggable={true} icon="☰" text="menu" handleClick={toggleModal}/>
     </div>
