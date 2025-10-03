f4870dc4709539f5db4da3dfaec61206ff0ebce3
Max Welin <maxwwelin@gmail.com>
2025-10-03 14:42:20 +0200

    merge conflic resolved, forgot to save file before pushing hehe

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,6 @@ import { useConfigContext } from "../hooks/useConfigContext";
 // import { useApiContext } from "../hooks/useApiContext";
 
 export default function HomePage() {
-<<<<<<< HEAD
   const {
     playerTurn,
     openModal,
@@ -19,12 +18,9 @@ export default function HomePage() {
     startGame,
     toggleModal,
   } = useMainContext();
-  const { boardTiles } = useConfigContext();
-=======
-  const { playerTurn, openModal, openMenu, placeMove, gameState, startGame, toggleModal } =
-    useMainContext();
-  const { boardTiles, rows, cols } = useConfigContext();
->>>>>>> 32c4b59 (added playPiece func in apiContext. Rows and Cols injected in homePage.jsx and passed to board as props)
+
+  const { rows, cols } = useConfigContext();
+
 
   {
     /* ignorera dessa, det är för att testa anrop mot backend :) */
@@ -50,7 +46,6 @@ export default function HomePage() {
         toggleModal={toggleModal}
       />
       <Board
-        boardTiles={boardTiles}
         boardRows={rows}
         boardCols={cols}
         playerTurn={playerTurn}
