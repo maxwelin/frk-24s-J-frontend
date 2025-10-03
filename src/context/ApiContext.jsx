73805a0a2805798f9df07b4a277f6c0ca838045d
Max Welin <maxwwelin@gmail.com>
2025-10-03 15:48:19 +0200

    added try catch block with error handling for all api call functions. Added a gameOnDonkeyKong(temp name) func which takes 2 player names, creates the players, creates a new game and joins that game with both players.

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -6,32 +6,101 @@ function ApiProvider({ children }) {
 
   const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
-  const addGame = async () => {
-    console.log(import.meta.env.VITE_API_BASE_URL + import.meta.env.VITE_CREATE_GAME_ENDPOINT)
-    const res = await fetch(`${BASE_URL}${import.meta.env.VITE_CREATE_GAME_ENDPOINT}`)
-    const data = await res.json()
-    console.log(data)
+  const createGame = async () => {
+    try {
+      const res = await fetch(`${BASE_URL}${import.meta.env.VITE_CREATE_GAME_ENDPOINT}`)
+      const data = await res.json()
+      
+      if(!res.ok) {
+        throw new Error(`Error creating game: ${res.status}`)
+      }
+      
+      console.log(data)
+      return data.id
+
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const createPlayer = async (name) => {
-    const res = await fetch(`${BASE_URL}${import.meta.env.VITE_CREATE_PLAYER_ENDPOINT}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        name: name
+    try {
+      const res = await fetch(`${BASE_URL}${import.meta.env.VITE_CREATE_PLAYER_ENDPOINT}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name: name
+        })
       })
-    })
-    const data = await res.json()
-    console.log(data)
+
+      const data = await res.json()
+
+      if(!res.ok) {
+        throw new Error(`Error creating player: ${res.status}`);
+      }
+      
+      console.log(data)
+     
+      return data.id
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const joinGame = async (gameId, playerId) => {
+    try {
+      const res = await fetch(`${BASE_URL}${import.meta.env.VITE_JOIN_GAME_ENDPOINT}/${gameId}/${playerId}`)
+      const data = await res.json()
+      
+      if(!res.ok) {
+        throw new Error(`Error joining game: ${res.status}`)
+      }
+      
+      console.log(data)
+      
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  
+  const placePiece = async (gameId, playerId, piecePosition) => {
+    try {
+      const res = await fetch(`${BASE_URL}${import.meta.env.VITE_PLAY_ENDPOINT}/${gameId}/${playerId}/${piecePosition.col}/${piecePosition.row}`)
+      const data = await res.json()
+      
+      if(!res.ok) {
+        throw new Error(`Error placing piece: ${res.status}`)
+      }
+      
+      console.log(data)
+      
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const gameOnDonkeyKong = async (player1Name, player2Name) => {
+    const p1 = await createPlayer(player1Name)
+    console.log("player one id: ", p1)
+    const p2 = await createPlayer(player2Name)
+    console.log("player two id: ", p2)
+    const gameId = await createGame()
+    console.log("game id: ", gameId)
+    await joinGame(gameId, p1)
+    await joinGame(gameId, p2)
   }
 
   return (
     <ApiContext.Provider
       value={{
-        addGame,
-        createPlayer
+        createGame,
+        createPlayer,
+        joinGame,
+        placePiece,
+        gameOnDonkeyKong
       }}
     >
       {children}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,7 @@ import { BackgroundBanner } from "@masewe/components";
 import "./HomePage.css";
 import { useMainContext } from "../hooks/useMainContext";
 import { useConfigContext } from "../hooks/useConfigContext";
-{
-  /* ignorera dessa imports, det är för att testa anrop mot backend :) */
-}
-// import { useApiContext } from "../hooks/useApiContext";
+
 
 export default function HomePage() {
   const {
@@ -20,16 +17,6 @@ export default function HomePage() {
   } = useMainContext();
 
   const { rows, cols } = useConfigContext();
-
-
-  {
-    /* ignorera dessa, det är för att testa anrop mot backend :) */
-  }
-  // const { addGame, createPlayer } = useApiContext();
-
-  // const onCreate = () => {
-  //   createPlayer("hej")
-  // }
 
   return (
     <div>
@@ -53,9 +40,6 @@ export default function HomePage() {
         gameState={gameState}
       />
       <Button draggable={true} icon="☰" text="menu" handleClick={openMenu} />
-      {/* ignorera dessa knappar, det är för att testa anrop mot backend :) */}
-      {/* <button onClick={addGame}>create game</button>
-      <button onClick={onCreate}>create player</button> */}
     </div>
   );
 }
